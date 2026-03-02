dc37e32940327084d82ca3c915e4418f68d7a840
Klemjl <juliaklemenchuck06@gmail.com>
2026-03-02 22:08:44 +0200

    Fixed error

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -14,9 +14,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(8, 1) == [8]
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(17, 4) == sorted(split_integer(17, 4))
-    assert max(split_integer(17, 4)) - min(split_integer(17, 4)) <= 1
+def test_split_integer_with_uneven_parts() -> None:
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
